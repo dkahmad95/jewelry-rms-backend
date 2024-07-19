769463faa510603638332055ceca6700d7d6fb9f
dkahmad95 <dkahmad95@gmail.com>
2024-07-19 18:44:43 +0300

    pre-deplyment5

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -3,6 +3,7 @@ const allowedOrigins: string[] = [
   'http://localhost:3000',
   'http://127.0.0.1:8080',
   'https://jewelry-management-system.vercel.app',
+  'https://jewelry-rms-backend1.onrender.com',
 ];
 
 export default allowedOrigins;
