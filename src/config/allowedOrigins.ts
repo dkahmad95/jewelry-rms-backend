ac928a92be121509d0e590c0531ac31c6c71c0e5
dkahmad95 <dkahmad95@gmail.com>
2024-07-19 18:33:39 +0300

    pre-deplyment5

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -2,6 +2,7 @@ const allowedOrigins: string[] = [
   'http://127.0.0.1:3000',
   'http://localhost:3000',
   'http://127.0.0.1:8080',
+  'https://jewelry-management-system.vercel.app/',
 ];
 
 export default allowedOrigins;
